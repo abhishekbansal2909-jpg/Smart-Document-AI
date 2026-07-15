a05309c2f3180e4559611cf5411eec07051bf73f
abhishekbansal2909-jpg <abhishekbansal2909@gmail.com>
2026-07-15 15:09:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ st.write("Upload a PDF document to extract structured data.")
 
 try:
   genai.configure(api_key = st.secrets["GEMINI_API_KEY"])
-  model = genai.GenerativeModel('gemini-1.5-flash')
+  model = genai.GenerativeModel('gemini-2.0-flash')
 except KeyError:
   st.error("API Key not found. Please ensure GEMINI_API_KEY is set in Streamlit Secrets.")
   st.stop()
